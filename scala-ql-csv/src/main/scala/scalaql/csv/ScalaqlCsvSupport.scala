3311913e56108910aee1e00425f8d5a2a6e44d6e
Vitalii <vitalii.honta@gmail.com>
2022-06-15 01:09:31 +0200

    Improved data source api ergonomics (#5)
    * Improved data source api ergonomics
    
    * A little bit more improving reader

--- scala-ql-csv/src/main/scala/scalaql/csv/ScalaqlCsvSupport.scala
+++ scala-ql-csv/src/main/scala/scalaql/csv/ScalaqlCsvSupport.scala
@@ -6,8 +6,13 @@ import scalaql.SideEffect
 import scalaql.sources.DataSourceReadSupport
 import scalaql.sources.DataSourceSupport
 import scalaql.sources.DataSourceWriteSupport
+
+import java.io.ByteArrayOutputStream
+import java.io.OutputStreamWriter
+import java.io.Reader
 import java.io.StringReader
 import java.io.StringWriter
+import java.io.Writer
 import java.nio.charset.Charset
 import java.nio.charset.StandardCharsets
 import java.nio.file.Files
@@ -18,60 +23,20 @@ import scala.collection.mutable
 trait ScalaqlCsvSupport extends DataSourceSupport[CsvDecoder.Row, CsvEncoder.Row, CsvConfig] {
 
   final object read extends DataSourceReadSupport[CsvDecoder.Row, CsvConfig] {
-    override def file[A: CsvDecoder.Row](
-      path:               Path,
-      encoding:           Charset
-    )(implicit csvConfig: CsvConfig
-    ): Iterable[A] =
-      readFrom {
-        CSVReader.open(path.toFile, encoding.name)(csvConfig.toTototoshi)
-      }
-
-    override def string[A: CsvDecoder.Row](
-      content:         String
-    )(implicit config: CsvConfig
-    ): Iterable[A] =
-      readFrom {
-        CSVReader.open(new StringReader(content))(config.toTototoshi)
-      }
-
-    private def readFrom[A: CsvDecoder.Row](reader: CSVReader): Iterable[A] =
-      reader.iteratorWithHeaders
+    override protected def readImpl[A: CsvDecoder.Row](reader: Reader)(implicit config: CsvConfig): Iterable[A] =
+      CSVReader
+        .open(reader)(config.toTototoshi)
+        .iteratorWithHeaders
         .map(raw => implicitly[CsvDecoder.Row[A]].read(CsvEntry.Row(raw)))
         .toList
   }
 
   final object write extends DataSourceWriteSupport[CsvEncoder.Row, CsvConfig] {
 
-    override def file[A: CsvEncoder.Row](
-      path:            Path,
-      encoding:        Charset,
-      openOptions:     OpenOption*
-    )(implicit config: CsvConfig
-    ): SideEffect[?, ?, A] =
-      writeInto {
-        val writer = Files.newBufferedWriter(path, encoding, openOptions: _*)
-        new CSVWriter(writer)(config.toTototoshi)
-      }.afterAll((writer, _) => writer.flush())
-
-    override def string[A: CsvEncoder.Row](
-      builder:         mutable.StringBuilder
-    )(implicit config: CsvConfig
-    ): SideEffect[?, ?, A] = {
-      val writer = new StringWriter()
-      writeInto {
-        CSVWriter.open(writer)(config.toTototoshi)
-      } onExit {
-        builder.append(writer)
-      }
-    }
-
-    private def writeInto[A: CsvEncoder.Row](
-      acquireWriter: => CSVWriter
-    ): SideEffect[CSVWriter, Boolean, A] =
+    override def write[A: CsvEncoder.Row](writer: => Writer)(implicit config: CsvConfig): SideEffect[?, ?, A] =
       SideEffect[CSVWriter, Boolean, A](
         initialState = false,
-        acquire = () => acquireWriter,
+        acquire = () => CSVWriter.open(writer)(config.toTototoshi),
         release = (writer, _) => writer.close(),
         (writer, writtenHeaders, value) => {
           val result = implicitly[CsvEncoder.Row[A]].write(value)
--- scala-ql-json/src/main/scala/scalaql/json/ScalaqlJsonSupport.scala
+++ scala-ql-json/src/main/scala/scalaql/json/ScalaqlJsonSupport.scala
@@ -8,6 +8,9 @@ import io.circe.parser
 import scalaql.sources.DataSourceReadSupport
 import scalaql.sources.DataSourceSupport
 import scalaql.sources.DataSourceWriteSupport
+import scala.jdk.CollectionConverters._
+import java.io.BufferedReader
+import java.io.Reader
 import java.io.StringWriter
 import java.io.Writer
 import java.nio.charset.Charset
@@ -15,70 +18,38 @@ import java.nio.charset.StandardCharsets
 import java.nio.file.Files
 import java.nio.file.OpenOption
 import java.nio.file.Path
+import java.util.stream.Collectors
 import scala.collection.mutable
 import scala.io.Source
 
 trait ScalaqlJsonSupport extends DataSourceSupport[Decoder, Encoder, JsonConfig] {
 
   final object read extends DataSourceReadSupport[Decoder, JsonConfig] {
-    override def file[A: Decoder](
-      path:            Path,
-      encoding:        Charset
-    )(implicit config: JsonConfig
-    ): Iterable[A] =
-      readFromSource {
-        Source.fromFile(path.toFile, encoding.name)
-      }
-
-    override def string[A: Decoder](
-      content:         String
-    )(implicit config: JsonConfig
-    ): Iterable[A] =
-      readFromSource {
-        Source.fromString(content)
-      }
-
-    private def readFromSource[A: Decoder](reader: Source)(implicit jsonConfig: JsonConfig): Iterable[A] =
-      if (jsonConfig.multiline) {
-        reader
-          .getLines()
-          .map(parser.decode[A](_).toTry.get)
+    protected def readImpl[A: Decoder](reader: Reader)(implicit config: JsonConfig): Iterable[A] = {
+      val bufferedReader = new BufferedReader(reader)
+      if (config.multiline) {
+        bufferedReader
+          .lines()
+          // java lambdas, need types
+          .map[A]((line: String) => parser.decode[A](line).toTry.get)
+          .collect(Collectors.toList[A])
+          .asScala
           .toList
       } else {
-        parser.decode[List[A]](reader.mkString).toTry.get
+        val content = bufferedReader
+          .lines()
+          .collect(Collectors.joining())
+
+        parser.decode[List[A]](content).toTry.get
       }
+    }
   }
 
   final object write extends DataSourceWriteSupport[Encoder, JsonConfig] {
-
-    override def file[A: Encoder](
-      path:            Path,
-      encoding:        Charset,
-      openOptions:     OpenOption*
-    )(implicit config: JsonConfig
-    ): SideEffect[?, ?, A] = writeInto {
-      Files.newBufferedWriter(path, encoding, openOptions: _*)
-    }.afterAll((writer, _) => writer.flush())
-
-    override def string[A: Encoder](
-      builder:         mutable.StringBuilder
-    )(implicit config: JsonConfig
-    ): SideEffect[?, ?, A] = {
-      val writer = new StringWriter()
-      writeInto {
-        writer
-      } onExit {
-        builder.append(writer)
-      }
-    }
-
-    private def writeInto[A: Encoder](
-      acquireWriter:   => Writer
-    )(implicit config: JsonConfig
-    ): SideEffect[Writer, ?, A] = {
+    override def write[A: Encoder](writer: => Writer)(implicit config: JsonConfig): SideEffect[?, ?, A] = {
       def basics(writeLine: (Writer, Boolean, Json) => Unit) = SideEffect[Writer, Boolean, A](
         initialState = true,
-        acquire = () => acquireWriter,
+        acquire = () => writer,
         release = (writer, _) => writer.close(),
         use = (writer, isFirstRow, value) => {
           writeLine(writer, isFirstRow, Encoder[A].apply(value))
@@ -97,9 +68,7 @@ trait ScalaqlJsonSupport extends DataSourceSupport[Decoder, Encoder, JsonConfig]
             writer.write(",")
           }
           writer.write(json.spaces2)
-        }
-          .beforeAll(_.write("[\r\n"))
-          .afterAll((writer, _) => writer.write("\r\n]"))
+        }.beforeAll(_.write("[\r\n")).afterAll((writer, _) => writer.write("\r\n]"))
       }
     }
   }
