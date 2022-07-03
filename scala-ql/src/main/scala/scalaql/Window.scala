23e070120420cb158b6b9d1c47d61ef6d71277dd
Vitalii Honta <vitalii.honta@gmail.com>
2022-07-03 22:30:32 +0200

    [#42] improved window functions syntax

--- scala-ql/src/main/scala/scalaql/Window.scala
+++ scala-ql/src/main/scala/scalaql/Window.scala
@@ -17,20 +17,76 @@ class Window[A] private (
       orderTags
     )
 
+  def partitionBy[B: Tag, C: Tag](f1: A => B, f2: A => C): Window[A] =
+    new Window[A](
+      f2 :: f1 :: partitions,
+      Tag[C].tag :: Tag[B].tag :: partitionTags,
+      orders,
+      orderTags
+    )
+
+  def partitionBy[B: Tag, C: Tag, D: Tag](f1: A => B, f2: A => C, f3: A => D): Window[A] =
+    new Window[A](
+      f3 :: f2 :: f1 :: partitions,
+      Tag[D].tag :: Tag[C].tag :: Tag[B].tag :: partitionTags,
+      orders,
+      orderTags
+    )
+
+  def partitionBy[B: Tag, C: Tag, D: Tag, E: Tag](f1: A => B, f2: A => C, f3: A => D, f4: A => E): Window[A] =
+    new Window[A](
+      f4 :: f3 :: f2 :: f1 :: partitions,
+      Tag[E].tag :: Tag[D].tag :: Tag[C].tag :: Tag[B].tag :: partitionTags,
+      orders,
+      orderTags
+    )
+
   def orderBy[B: Tag: Ordering](f: A => B): Window[A] =
     new Window[A](
       partitions,
       partitionTags,
-      (f, Ordering[B].asInstanceOf[Ordering[Any]]) :: orders,
+      makeOrder(f) :: orders,
       Tag[B].tag :: orderTags
     )
 
-  def getPartitionKey(value: A): Int =
+  def orderBy[B: Tag: Ordering, C: Tag: Ordering](f1: A => B, f2: A => C): Window[A] =
+    new Window[A](
+      partitions,
+      partitionTags,
+      makeOrder(f2) :: makeOrder(f1) :: orders,
+      Tag[C].tag :: Tag[B].tag :: orderTags
+    )
+
+  def orderBy[B: Tag: Ordering, C: Tag: Ordering, D: Tag: Ordering](f1: A => B, f2: A => C, f3: A => D): Window[A] =
+    new Window[A](
+      partitions,
+      partitionTags,
+      makeOrder(f3) :: makeOrder(f2) :: makeOrder(f1) :: orders,
+      Tag[D].tag :: Tag[C].tag :: Tag[B].tag :: orderTags
+    )
+
+  def orderBy[B: Tag: Ordering, C: Tag: Ordering, D: Tag: Ordering, E: Tag: Ordering](
+    f1: A => B,
+    f2: A => C,
+    f3: A => D,
+    f4: A => E
+  ): Window[A] =
+    new Window[A](
+      partitions,
+      partitionTags,
+      makeOrder(f4) :: makeOrder(f3) :: makeOrder(f2) :: makeOrder(f1) :: orders,
+      Tag[E].tag :: Tag[D].tag :: Tag[C].tag :: Tag[B].tag :: orderTags
+    )
+
+  private[scalaql] def getPartitionKey(value: A): Int =
     MurmurHash3.orderedHash(
       partitions.map(_.apply(value)).reverse
     )
 
-  def ordering: Ordering[A] = new Window.ChainedOrdering[A](orders.reverse)
+  private[scalaql] def ordering: Ordering[A] = new Window.ChainedOrdering[A](orders.reverse)
+
+  private def makeOrder[B: Ordering](f: A => B): (A => Any, Ordering[Any]) =
+    (f, Ordering[B].asInstanceOf[Ordering[Any]])
 }
 
 object Window {
